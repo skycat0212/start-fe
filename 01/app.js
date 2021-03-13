3b9e7386a337f0422836c790b0a6aff5e7c62899
skycat0212 <mr_magnet@naver.com>
2021-03-13 19:46:50 +0900

    01 수업 과제 (2주차 수업)

---
+++ 01/app.js
@@ -0,0 +1,155 @@
+console.log('app.js');
+
+// // 변수
+// var name = '강순범';
+// var Name = '강순범';
+// var name = '강순범'; // 같은 이름의 변수를 써도 에러가 나지 않음
+
+// console.log(name);
+// console.log(Name);
+
+
+
+// // 예약어 -> 예약어는 변수명을 사용할 수 없음
+// var var = 'test';
+// console.log(var);
+
+
+
+// // 주석
+// // 한줄 주석
+// console.log(1)
+// console.log(2)
+
+// /*
+// // 긴 주석
+// console.log(1)
+// console.log(2)
+// */
+
+
+
+// 데이터 타입
+// Stirng
+
+// // Number
+// var count = 1;
+// console.log(count);
+
+
+// // Boolean - true or false 쌍따옴표 사용 안함
+// var isMan = true; // bool
+// var isMan = "true"; // String
+
+// console.log(isMan);
+
+
+// Function
+// function log(str) {
+//     console.log(str);
+// }
+
+// log('hello');
+
+// var log = function(str) {
+//     console.log(str)
+// }
+// log('hello');
+
+
+// // Object
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+
+// image.length = 7
+// image['type'] = 'png'
+
+// image = {w:1, h:2}
+
+// // var imageWidth = 100;
+// // var imageHeight = 100;
+// // var imageName = '1.png';
+
+// console.log(image);
+
+
+// // Array
+// var image1 = [1,2,3,4,5,6,"a",{}];
+// image1[0] = 'a'
+// image1[8] = '8th'
+// console.log(image1);
+
+
+// //null
+// var data = null;
+// var data1 = '';
+// console.log(data,data1);
+
+
+// //undefined
+// var data2;
+// console.log(data2);
+
+
+
+// // 주요 연산자
+// +
+// console.log(1 + 2);
+// console.log('a' + 'b');
+// console.log(1 + '4'); // 데이터타입 섞어서 쓰는건 지양해야 함
+
+// var count = 1;
+// var number = '2';
+
+// console.log(count + number);
+
+
+// // 동등, 일치
+// console.log(1 == 1); // true
+// console.log(1 == 2); // false
+// console.log('' == '0'); // false
+// console.log(0 == ''); // true 0 -> false, '' -> false
+// console.log(0 === ''); // false
+
+
+// //
+// if (true) {
+//     console.log(1);
+// }
+// if (false) {
+//     console.log(2);
+// }
+
+
+// // and, or, !
+// var isHero = true;
+
+// if (isHero) {
+//     console.log('hero');
+// }
+// if (!isHero) {
+//     console.log('hero');
+// }
+
+
+// // typeof
+// console.log(typeof 1);
+// console.log(typeof []);
+// console.log(typeof '1');
+// console.log(typeof {});
+
+
+
+
+
+
+
+
+
+
+
+
+
