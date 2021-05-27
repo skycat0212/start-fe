78caad3137bf6885616755f73e434d612b672445
skycat0212 <mr_magnet@naver.com>
2021-05-27 23:00:52 +0900

    11(12주차 수업)

---
+++ 11/src/form-input.js
@@ -0,0 +1,28 @@
+import todomodel from './todo-model';
+import { render } from './html-render';
+
+const $inputForm = document.querySelector('#input-form');
+const $input = document.querySelector('#input');
+
+function init(data) {
+    $inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const text = $input.value;
+        if (!text) return;
+
+        $input.value = '';
+
+        data.push({
+            ...todomodel,
+            text,
+            isDone: false,
+        });
+
+        render(data);
+    });
+}
+
+export default {
+    init,
+};
