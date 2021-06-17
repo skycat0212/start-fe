622262f8deb056df948c0b2b09fec6767577a986
skycat0212 <mr_magnet@naver.com>
2021-06-17 13:29:00 +0900

    13

---
+++ 13/react.js
@@ -0,0 +1,15 @@
+// const rootEl = document.getElementById('root');
+// // const el = document.createElement('div');
+// // el.textContent = 'React';
+// // el.className = 'container';
+// // rootEl.append(el);
+
+// // const el = React.createElement('div', { className: 'container' }, 'React');
+// // ReactDOM.render(el, rootEl);
+
+// const element = <h1>Hello, {name}</h1>;
+
+
+const rootEl = document.getElementById('root');
+const el = <div className="container">React</div>;
+ReactDOM.render(el, rootEl);
